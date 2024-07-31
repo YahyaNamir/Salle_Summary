51d24ac89a4f9d55d5fe37d33b43b0c002aac715
YAHYA NAMIR <132499945+YahyaNamir@users.noreply.github.com>
2024-07-31 14:48:57 +0100

    Chemistry Item

--- components/ChemistryItem.js
+++ components/ChemistryItem.js
@@ -5,7 +5,7 @@ export default function ChemistryItem({ chemistry }) {
   return (
     <View style={styles.itemContainer}>
       <Text style={{fontFamily: 'Poppins-Bold', marginLeft: 5}} >{chemistry.name}</Text>
-      <Text style={{fontFamily: 'Poppins-Bold', marginLeft: 5}} >{chemistry.minutePlayer}</Text>
+      {/* <Text style={{fontFamily: 'Poppins-Bold', marginLeft: 5}} >{chemistry.minutePlayer}</Text> */}
       {/* <Text style={{fontFamily: 'Poppins-Bold', marginLeft: 5}} >{chemistry.goal}</Text> */}
       {/* <Text style={{fontFamily: 'Poppins-Bold', marginLeft: 5}} >{chemistry.receivedGoal}</Text> */}
       {/* <Text style={{fontFamily: 'Poppins-Bold', marginLeft: 5}} >{chemistry.yellowCard}</Text> */}
