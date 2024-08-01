258aaf17fa5a2da94d1d0e32fb7f24dd1acd3007
YAHYA NAMIR <132499945+YahyaNamir@users.noreply.github.com>
2024-08-01 16:47:03 +0100

    comment it

--- services/i18next.js
+++ services/i18next.js
@@ -1,20 +1,20 @@
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
+// import i18next from "i18next";
+// import { initReactI18next } from "react-i18next";
 
-import en from "../locales/en/en.json";
-import fr from "../locales/fr/fr.json";
+// import en from "../locales/en/en.json";
+// import fr from "../locales/fr/fr.json";
 
-export const languageResources = {
-    en : {Translation: en},
-    fr : {Translation: fr}
-}
+// export const languageResources = {
+//     en : {Translation: en},
+//     fr : {Translation: fr}
+// }
 
 
-i18next.use(initReactI18next).init({
-    compatibilityJSON: 'v3',
-    lang : 'en',
-    fallbackLng : 'en',
-    resources : languageResources,
-})
+// i18next.use(initReactI18next).init({
+//     compatibilityJSON: 'v3',
+//     lang : 'en',
+//     fallbackLng : 'en',
+//     resources : languageResources,
+// })
 
-export default i18next;
+// export default i18next;
